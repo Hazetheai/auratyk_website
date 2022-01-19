0be905dd1f6dcc7640e1d43a7d3779bb492cda40
Jake Riordan <contact@jakeriordan.dev>
2022-01-19 17:59:36 +0100

    init(app): base config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,25 +4,30 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'auratyk',
+    title: 'Auratyk',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [
+      {
+        src: 'https://unpkg.com/codyhouse-framework/main/assets/js/util.js',
+        async: true,
+      },
+    ],
+    htmlAttrs: {
+      class: 'js',
+    },
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: ['@/assets/scss/app.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -30,8 +35,18 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    // '@nuxtjs/eslint-module',
+    // https://www.npmjs.com/package/@nuxtjs/style-resources
+    '@nuxtjs/style-resources',
   ],
+
+  styleResources: {
+    scss: [
+      '~assets/scss/resources/_mixins.scss',
+      '~assets/scss/resources/_breakpoints.scss',
+    ],
+    hoistUseStatements: true, // Hoists the "@use" imports. Applies only to "sass", "scss" and "less". Default: false.
+  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -41,6 +56,7 @@ export default {
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
+    ['@nuxtjs/style-resources'],
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -52,14 +68,13 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
-    }
+      lang: 'en',
+    },
   },
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <Tutorial/>
+  <MainHeader1 />
 </template>
 
 <script>
+import MainHeader1 from '../components/G-MainHeader-1.vue'
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: { MainHeader1 },
 }
 </script>
