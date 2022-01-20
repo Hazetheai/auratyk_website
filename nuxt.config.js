38263dbc1e0530f976ca4ec83fc60375872809fc
Jake Riordan <contact@jakeriordan.dev>
2022-01-20 14:48:10 +0100

    setup(config): Add template config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,15 @@
+import webpack from 'webpack'
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0
+    const v = c === 'x' ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
+}
+
 export default {
+  version: uuidv4(),
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
@@ -27,10 +38,64 @@ export default {
   css: ['@/assets/scss/app.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: [
+    {
+      path: '@/components',
+      extensions: ['vue'],
+    },
+    {
+      path: '@/components/app',
+      prefix: 'app',
+      extensions: ['vue'],
+    },
+    {
+      path: '@/components/elements',
+      prefix: 'e',
+      extensions: ['vue'],
+    },
+    {
+      path: '@/components/blocks',
+      prefix: 'block',
+      extensions: ['vue'],
+    },
+    {
+      path: '@/components/webgl',
+      prefix: 'webgl',
+      extensions: ['vue'],
+    },
+    {
+      path: '@/components/svg',
+      prefix: 'svg',
+      extensions: ['vue'],
+    },
+  ],
+  pageTransition: {
+    name: 'page',
+    mode: 'out-in',
+    beforeLeave(el) {
+      // console.log('Before leave...')
+      this.$events.emit('cursor:enter', { type: 'default' })
+    },
+    leave(el) {
+      // console.log('Enter...')
+      this.$events.emit('scroller:reset')
+    },
+    afterLeave(el) {
+      // console.log('After leave...')
+    },
+    beforeEnter(el) {
+      // console.log('Before enter...')
+    },
+    enter(el) {
+      // console.log('Enter...')
+    },
+    afterEnter(el) {
+      // console.log('After enter...')
+    },
+  },
+  loading: false,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
@@ -44,6 +109,8 @@ export default {
     scss: [
       '~assets/scss/resources/_mixins.scss',
       '~assets/scss/resources/_breakpoints.scss',
+      '~assets/scss/resources/_hover.scss',
+      '~assets/scss/resources/_media.scss',
     ],
     hoistUseStatements: true, // Hoists the "@use" imports. Applies only to "sass", "scss" and "less". Default: false.
   },
@@ -56,8 +123,20 @@ export default {
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
-    ['@nuxtjs/style-resources'],
+    '@nuxtjs/style-resources',
+    'nuxt-mq',
+    'nuxt-helmet',
+    'nuxt-ssr-cache',
+    'nuxt-compress',
   ],
+
+  mq: {
+    defaultBreakpoint: 'default',
+    breakpoints: {
+      mobile: 769,
+      desktop: Infinity,
+    },
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
@@ -75,6 +154,42 @@ export default {
   // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
 
+  plugins: [
+    { src: '~/plugins/polyfills.js', mode: 'client' },
+    { src: '~/plugins/events.js', mode: 'client' },
+    { src: '~/plugins/frame.js', mode: 'client' },
+    { src: '~/plugins/viewport.js', mode: 'client' },
+    { src: '~/plugins/mouse.js', mode: 'client' },
+    { src: '~/plugins/directives.js', mode: 'client' },
+    { src: '~/plugins/gsap.js', mode: 'client' },
+  ],
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    extend: (config) => {
+      config.plugins.push(new webpack.ProvidePlugin({ THREE: 'three' }))
+      config.module.rules.push({
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: ['raw-loader', 'glslify-loader'],
+      })
+    },
+    babel: {
+      plugins: [
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/proposal-class-properties',
+        '@babel/plugin-proposal-optional-chaining',
+      ],
+      presets({ isServer }) {
+        return [
+          [
+            '@babel/preset-env',
+            {
+              targets: '> 2%, not dead',
+            },
+          ],
+        ]
+      },
+    },
+  },
 }
