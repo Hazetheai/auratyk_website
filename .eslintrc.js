c0bf36647d5187141170aface4c006e46d4de78b
Jake Riordan <contact@jakeriordan.dev>
2022-01-20 21:27:36 +0100

    fix(eslint): reenable

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,11 +8,7 @@ module.exports = {
     parser: '@babel/eslint-parser',
     requireConfigFile: false,
   },
-  extends: [
-    // '@nuxtjs',
-    // 'plugin:nuxt/recommended',
-    // 'prettier'
-  ],
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
   plugins: [],
   // add your custom rules here
   rules: {},
