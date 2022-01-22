750265c5539a4caab4eb6a0110662a277c86a5a4
Jake Riordan <contact@jakeriordan.dev>
2022-01-22 15:52:22 +0100

    chore(env): formatting

--- assets/js/classes/ParticleClass.js
+++ assets/js/classes/ParticleClass.js
@@ -1,48 +1,48 @@
-import * as THREE from "three";
+import * as THREE from 'three'
 
 class ParticleSystem {
-  constructor() {
-    this.bind();
-    this.particleCount = 5000;
-    this.boxSize = 25;
+  constructor(particleCount = 1000, boxSize = 15) {
+    this.bind()
+    this.particleCount = particleCount
+    this.boxSize = boxSize
   }
 
   init(scene) {
-    this.scene = scene;
-    this.particlesGeom = new THREE.BufferGeometry();
-    this.particlesPos = [];
+    this.scene = scene
+    this.particlesGeom = new THREE.BufferGeometry()
+    this.particlesPos = []
 
     for (let p = 0; p < this.particleCount; p++) {
-      let x = Math.random() * this.boxSize - this.boxSize / 2;
-      let y = Math.random() * this.boxSize - this.boxSize / 2;
-      let z = Math.random() * this.boxSize - this.boxSize / 2;
+      let x = Math.random() * this.boxSize - this.boxSize / 2
+      let y = Math.random() * this.boxSize - this.boxSize / 2
+      let z = Math.random() * this.boxSize - this.boxSize / 2
 
       // Create the vertex
-      this.particlesPos.push(x, y, z);
+      this.particlesPos.push(x, y, z)
     }
 
     this.particlesGeom.setAttribute(
-      "position",
+      'position',
       new THREE.Float32BufferAttribute(this.particlesPos, 3)
-    );
+    )
 
     this.particleMaterial = new THREE.PointsMaterial({
       color: 0xffffff,
       size: 0.015,
-    });
+    })
 
     this.particleSystem = new THREE.Points(
       this.particlesGeom,
       this.particleMaterial
-    );
+    )
     // console.log(this.particlesGeom.attributes.position.array);
-    this.scene.add(this.particleSystem);
+    this.scene.add(this.particleSystem)
   }
 
   update() {
-    let i = 0;
+    let i = 0
     while (i < this.particleCount) {
-      const particleArr = this.particlesGeom.attributes.position.array;
+      const particleArr = this.particlesGeom.attributes.position.array
       //   const x = (particleArr[i * 3 + 0] += 0.01);
       // prettier-ignore
       const y = (particleArr[i * 3 + 1] += 0.01);
@@ -52,19 +52,19 @@ class ParticleSystem {
           // prettier-ignore
         particleArr[i * 3 + 1]  = -this.boxSize / 2;
       }
-      i++;
+      i++
     }
 
-    this.particlesGeom.attributes.position.needsUpdate = true;
+    this.particlesGeom.attributes.position.needsUpdate = true
   }
 
   bind() {
-    this.init = this.init.bind(this);
-    this.update = this.update.bind(this);
+    this.init = this.init.bind(this)
+    this.update = this.update.bind(this)
   }
 }
 
-const _instance = new ParticleSystem();
+const _instance = new ParticleSystem()
 
-export { ParticleSystem };
-export default _instance;
+export { ParticleSystem }
+export default _instance
