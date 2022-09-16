5e84007febc17deae95633469316cb7a0a50d663
hazetheai <contact@jakeriordan.dev>
2022-09-16 20:36:29 +0200

    fix(links): update SC link

--- assets/js/utils/getSocials.js
+++ assets/js/utils/getSocials.js
@@ -1,6 +1,6 @@
 export default {
   instagram: 'https://www.instagram.com/auratyk/',
-  soundcloud: 'https://soundcloud.com/user-618807717',
+  soundcloud: 'https://soundcloud.com/auratyk',
   bandcamp: 'https://auratyk.bandcamp.com/releases?external_follow=1',
   youtube: 'https://www.youtube.com/channel/UC3_B4pGVMt_u3SI4mJlLmrQ',
 }
