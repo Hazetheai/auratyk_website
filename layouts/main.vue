ffebcae5c0a8fcd2a42c2f8c445410efbbd968fc
hazetheai <contact@jakeriordan.dev>
2022-11-10 12:14:54 +0100

    fix(env): Prod & Staging checks

--- layouts/main.vue
+++ layouts/main.vue
@@ -17,18 +17,18 @@ export default {
 
   mounted() {
     const isProd = process.env.NODE_ENV === 'production'
-    const isProdDomain = /auratyk\.com/.test(window.location.hostname)
+    const isProdDomain = /(www\.)?auratyk\.com/.test(window.location.hostname)
     const isStagingDomain = /staging\.auratyk\.com/.test(
       window.location.hostname
     )
     const isLocalDomain = /localhost/.test(window.location.hostname)
 
-    if (isProd && /(www\.)?auratyk\.com/.test(window.location.hostname)) {
+    if (isProd && isProdDomain && !isStagingDomain) {
       this.$InsightsAnalytics.init('yES5xEiki5bhfgmw')
       this.$InsightsAnalytics.trackPages()
     }
     console.log('this.$route', this.$route)
-    console.log('Is Primary Prod Domain', isProdDomain)
+    console.log('Is Primary Prod Domain', isProdDomain && !isStagingDomain)
     console.log('Is Staging Domain', isStagingDomain)
     console.log('Is Local Version', isLocalDomain)
 
