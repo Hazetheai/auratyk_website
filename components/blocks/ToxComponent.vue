4012f0d7162c46fe8b9c1acf3778926748c0a128
hazetheai <contact@jakeriordan.dev>
2025-03-10 20:22:56 +0100

    feat: add video

--- components/blocks/ToxComponent.vue
+++ components/blocks/ToxComponent.vue
@@ -7,19 +7,17 @@
         <h1 class="main__content-heading text-center padding-bottom-lg">
           {{ release.name }} {{ release.type }}
         </h1>
-
-<!-- <pre>{{ JSON.stringify(release, null, 2) }}</pre> -->
-        <nuxt-img
+        <video
           class="dark-image"
           sizes="sm:100vw md:50vw lg:600px"
-          src="/images/cloud_seq-teaser.png"
-          alt="'Form' Cover Art"
-          style="
-    object-fit: cover;
-    max-height: 550px;
-    width: 100%;
-"
-        />
+          src="/video/cloud_seq-teaser.mp4"
+          alt="Cloud_Seq demo"
+          title="Cloud_Seq demo video"
+          controls
+          loop
+          preload="auto"
+          style="object-fit: cover; max-height: 550px; width: 100%"
+        ></video>
       </div>
     </div>
     <div class="text-divider padding-y-lg"></div>
@@ -29,12 +27,6 @@
       >
         <h3 class="">A Pointcloud sequencer for TouchDesigner</h3>
 
-        <!-- <NotionRenderer
-          v-if="notionContent.releasesBlockMap"
-          :blockMap="notionContent.releasesBlockMap"
-          :contentId="pressReleaseContentId"
-          class="max-width-md@md"
-        /> -->
         <div class="notion-sync-block max-width-md@md">
           <p class="notion-text">
             <span
@@ -48,71 +40,87 @@
             </span>
           </p>
           <h4 class="margin-top-sm"><strong>Features</strong></h4>
-          <p class="notion-text">
-                <ul>
-                <li class="margin-bottom-sm"><p><strong>Pointcloud Loading &amp; Normalization:</strong></p>
-                <p>  Imports .ply files and normalizes the data.</p>
-                </li>
-                <li class="margin-bottom-sm"><p><strong>Layout Texture Arrangement:</strong></p>
-                <p>  Arranges multiple pointclouds into a layout texture for spatial alignment.</p>
-                </li>
-                <li class="margin-bottom-sm"><p><strong>LOD-Style Point Selection:</strong></p>
-                <p>  Uses the camera direction to select and blend points efficiently from two textures.</p>
-                </li>
-                <li class="margin-bottom-sm"><p><strong>Dynamic Sequencing Parameters:</strong></p>
-                <p>  Custom parameters control sequencing, blending thresholds, and source swapping.</p>
-                </li>
-                </ul>
-
-          </p>
+          <div class="notion-text">
+            <ul>
+              <li class="margin-bottom-sm">
+                <p><strong>Pointcloud Loading &amp; Normalization:</strong></p>
+                <p>Imports .ply files and normalizes the data.</p>
+              </li>
+              <li class="margin-bottom-sm">
+                <p><strong>Layout Texture Arrangement:</strong></p>
+                <p>
+                  Arranges multiple pointclouds into a layout texture for
+                  spatial alignment.
+                </p>
+              </li>
+              <li class="margin-bottom-sm">
+                <p><strong>LOD-Style Point Selection:</strong></p>
+                <p>
+                  Uses the camera direction to select and blend points
+                  efficiently from two textures.
+                </p>
+              </li>
+              <li class="margin-bottom-sm">
+                <p><strong>Dynamic Sequencing Parameters:</strong></p>
+                <p>
+                  Custom parameters control sequencing, blending thresholds, and
+                  source swapping.
+                </p>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <div
         style="min-width: 50%"
         class="release-details__tickets flex justify-between padding-top-md padding-x-sm@md margin-bottom-sm gap-x-md"
       >
-        <a  class="link" style="width:50%" :href="release.compLink" rel="noopener" target="_blank" @click="logDownload('the Cloud_Seq tox')"
-          >
+        <a
+          class="link"
+          style="width: 50%"
+          :href="release.compLink"
+          rel="noopener"
+          target="_blank"
+          @click="logDownload('the Cloud_Seq tox')"
+        >
           <nuxt-img
-          
-          class="dark-image"
-          sizes="sm:100vw md:50vw lg:600px"
-          src="/images/cloud_seq-tox.png"
-          alt="'Form' Cover Art"
-          style="
-    object-fit: cover;
-    max-height: 550px;
-    width: 100%;
-"
+            class="dark-image"
+            sizes="sm:100vw md:50vw lg:600px"
+            src="/images/cloud_seq-tox.png"
+            alt="'Form' Cover Art"
+            style="object-fit: cover; max-height: 550px; width: 100%"
+          />
+          <br />
+          <br />
 
-        />
-          <br>
-          <br>
-          
-          Component</a>
-        <a class="link" style="width:54%" :href="release.exampleLink" rel="noopener" target="_blank"  @click="logDownload('the Cloud_Seq example file')"
-          >
-          
+          Component</a
+        >
+        <a
+          class="link"
+          style="width: 54%"
+          :href="release.exampleLink"
+          rel="noopener"
+          target="_blank"
+          @click="logDownload('the Cloud_Seq example file')"
+        >
           <nuxt-img
-          class="dark-image"
-          sizes="sm:100vw md:50vw lg:600px"
-          src="/images/cloud_seq-example.png"
-          alt="'Form' Cover Art"
-          style="object-fit: cover; max-height: 550px; width: 100%;"
-          
-        />
-        
-          <br>
-          <br>
-          Example File</a>
+            class="dark-image"
+            sizes="sm:100vw md:50vw lg:600px"
+            src="/images/cloud_seq-example.png"
+            alt="'Form' Cover Art"
+            style="object-fit: cover; max-height: 550px; width: 100%"
+          />
 
-      
+          <br />
+          <br />
+          Example File</a
+        >
       </div>
       <div
         style="min-width: 50%"
         class="release-details__tickets flex justify-center padding-top-md padding-x-sm@md margin-bottom-sm"
       >
-      <a
+        <a
           v-for="socialLink in release.socialLinks"
           :key="socialLink.url"
           class="link padding-right-sm inline-block"
@@ -124,13 +132,17 @@
             ><icon-instagram width="36" height="36" title="Follow on Instagram"
           /></span>
         </a>
-        <a href='https://ko-fi.com/Z8Z8169G8L' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi4.png?v=6' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
-
-      
+        <a href="https://ko-fi.com/Z8Z8169G8L" target="_blank"
+          ><img
+            height="36"
+            style="border: 0px; height: 36px"
+            src="https://storage.ko-fi.com/cdn/kofi4.png?v=6"
+            border="0"
+            alt="Buy Me a Coffee at ko-fi.com"
+        /></a>
       </div>
     </div>
     <div class="text-divider padding-y-lg"></div>
-
   </section>
 </template>
 
@@ -146,7 +158,7 @@ export default {
     release: { type: Object, default: () => ({}) },
   },
 
-  data() {  
+  data() {
     return {
       //   tracks: ['season_ending', 'skitter', 'epiderm', 'remember_linn'],
       bioContentId: bioContentIds.blocks.short,
